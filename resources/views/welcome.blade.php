37ea79f36326563e578ce460bf2c3eec1caa2928
Yutalina <yutalina@localhost.localdomain>
2019-12-27 14:31:56 -0500

    new branch dev

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    DEV
                 </div>
 
                 <div class="links">
