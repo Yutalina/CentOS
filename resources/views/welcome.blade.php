798c4873ff4be73410048bc4c07c8f85106c4da2
Yutalina <48585546+Yutalina@users.noreply.github.com>
2019-12-28 12:22:33 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    TEST
+                    TEST Update
                 </div>
 
                 <div class="links">
